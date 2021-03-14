d33e7ff8319ce37e83f413aad8690a2ef373b9d0
ippee <ippee.music@gmail.com>
2021-03-14 19:34:15 +0900

    add a message at hovering a icon

--- src/components/Editor/FontList.tsx
+++ src/components/Editor/FontList.tsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react';
 import { Radio, RadioGroup } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/react';
+import { Tooltip } from '@chakra-ui/react';
 
 import fontListJson, { FontInfo } from '../../assets/json/fontlist.json';
 
@@ -134,34 +135,40 @@ function FontList(props: FontListProps) {
               <p className="author">by {fontJson[i].author}</p>
             </div>
             <div className="buttons">
-              <Button
-                className="font-license"
-                variant="link"
-                onClick={() => { window.open(fontJson[i].license_link) }}
-              >
-                <i className="icon-id-card-o" />
-              </Button>
-              <Button
-                className="font-link"
-                variant="link"
-                onClick={() => { window.open(fontJson[i].website) }}
-              >
-                <i className="icon-link" />
-              </Button>
-              <label htmlFor={props.lang + '-fav-' + i}>
-                <input
-                  className="fav-button"
-                  id={props.lang + '-fav-' + i}
-                  type="checkbox"
-                  name="favrite"
-                  defaultChecked={favValue[index][i]}
-                  onChange={() => {
-                    updatefavValue(index, i);
-                    sortItems(index);
-                  }}
-                />
-                <i id={props.lang + 'fav-icon-' + i} className="icon-heart" />
-              </label>
+              <Tooltip label={fontJson[i].license} placement="top">
+                <Button
+                  className="font-license"
+                  variant="link"
+                  onClick={() => { window.open(fontJson[i].license_link) }}
+                >
+                  <i className="icon-id-card-o" />
+                </Button>
+              </Tooltip>
+              <Tooltip label={fontJson[i].website} placement="top">
+                <Button
+                  className="font-link"
+                  variant="link"
+                  onClick={() => { window.open(fontJson[i].website) }}
+                >
+                  <i className="icon-link" />
+                </Button>
+              </Tooltip>
+              <Tooltip label="Favorite!" placement="top">
+                <label htmlFor={props.lang + '-fav-' + i}>
+                  <input
+                    className="fav-button"
+                    id={props.lang + '-fav-' + i}
+                    type="checkbox"
+                    name="favrite"
+                    defaultChecked={favValue[index][i]}
+                    onChange={() => {
+                      updatefavValue(index, i);
+                      sortItems(index);
+                    }}
+                  />
+                  <i id={props.lang + 'fav-icon-' + i} className="icon-heart" />
+                </label>
+              </Tooltip>
             </div>
           </div>
         );})
