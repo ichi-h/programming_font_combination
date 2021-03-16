37950f50be32808349dd3e8713e3d572a290569b
ippee <ippee.music@gmail.com>
2021-03-16 20:04:45 +0900

    update CodeMirror settings

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -11,12 +11,12 @@ import 'codemirror/theme/base16-dark.css';
 import 'codemirror/theme/base16-light.css';
 import fontListJson from '../assets/json/fontlist.json';
 
-require('codemirror/mode/javascript/javascript.js');
+require('codemirror/mode/htmlmixed/htmlmixed.js');
 
 function Editor() {
-  const value = 'console.log("Hello!");';
+  const value = TextValue();
   const [theme, setTheme] = useState('base16-dark');
-  const [fontSize, setFontSize] = useState(18);
+  const [fontSize, setFontSize] = useState(20);
   const [currentFont, setCurrentFont] = useState([
     fontListJson.eng[0].name,
     fontListJson.jpn[0].name
@@ -50,7 +50,7 @@ function Editor() {
             <CodeMirror
               value={value}
               options={{
-                mode: 'javascript',
+                mode: 'htmlmixed',
                 theme: theme,
                 lineNumbers: true
               }}
@@ -60,6 +60,49 @@ function Editor() {
       </Grid>
     </div>
   );
+}
+
+function TextValue(): string {
+  return `<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>カウントボタン</title>
+</head>
+<body>
+  <div class="counter">
+    <h1>カウンターデモ</h1>
+    <input class="plus" type="button" value="+" onclick="counter.increment();" />
+    <div class="count" id="count">0</div>
+    <input class="minus" type="button" value="-" onclick="counter.decrement();" />
+  </div>
+
+  <script>
+    console.log("紛らわしい文字: 0Oo 1iIl rnm gq9 エ工 ー一");
+
+    console.log("リガチャ: -> => == != === !== <= >= && ||");
+
+    let counter = {
+      value: 0,
+      elm: document.getElementById("count"),
+      increment: function() {
+        // 1クリックごとに1増加
+        this.value++;
+        this.elm.innerText = this.value;
+      },
+      decrement: function() {
+        // 1クリックごとに1減少
+        this.value--;
+        this.elm.innerText = this.value;
+      }
+    };
+
+    console.log("Ligature: -> => == != === !== <= >= && ||");
+  </script>
+</body>
+</html>`
 }
 
 export default Editor;
