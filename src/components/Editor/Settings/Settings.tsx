9d3f4a2b6a4b0f12fcf16eca037f10c93048ae57
ippee <ippee.music@gmail.com>
2021-03-18 18:02:39 +0900

    add the GitHub button

--- src/components/Editor/Settings/Settings.tsx
+++ src/components/Editor/Settings/Settings.tsx
@@ -1,6 +1,5 @@
-import { Grid, GridItem } from '@chakra-ui/react';
 import { Select } from '@chakra-ui/react';
-import { Center } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import {
   NumberInput,
   NumberInputField,
@@ -18,59 +17,51 @@ function Settings() {
   const [fontSizeValue, themeValue, updateSettings] = useSettingsStore();
 
   return (
-    <div className="settings">
-      <Grid
-        templateRows="repeat(1, 1fr)"
-        templateColumns="repeat(12, 1fr)"
-        gap={5}
-      >
-        <GridItem colSpan={1}>
-          <Center h="6vh">
-            <p className="setting-item">Font Size</p>
-          </Center>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <Center h="6vh">
-            <NumberInput
-              className="setting-item"
-              size="sm"
-              value={fontSizeValue}
-              min={1}
-              onChange={(e) => { updateSettings({
-                message: 'ChangeFontSize',
-                newFontSize: Number(e)
-              })}}
-            >
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </Center>
-        </GridItem>
+    <div className="navbar">
+      <div className="settings">
 
-        <GridItem colSpan={1}>
-          <Center h="6vh">
-            <p className="setting-item">Theme</p>
-          </Center>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <Center h="6vh">
-            <Select
-              size="sm"
-              defaultValue={themeValue}
-              onChange={(e) => { updateSettings({
-                message: 'ChangeTheme',
-                newTheme: e.target.value as Theme
-              })}}
-            >
-              <option value="base16-dark">base16-dark</option>
-              <option value="base16-light">base16-light</option>
-            </Select>
-          </Center>
-        </GridItem>
-      </Grid>
+        <p className="setting-label">Font Size</p>
+        <NumberInput
+          className="setting-item"
+          size="sm"
+          value={fontSizeValue}
+          min={1}
+          onChange={(e) => { updateSettings({
+            message: 'ChangeFontSize',
+            newFontSize: Number(e)
+          })}}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+
+        <p className="setting-label">Theme</p>
+        <Select
+          className="setting-item"
+          size="sm"
+          defaultValue={themeValue}
+          onChange={(e) => { updateSettings({
+            message: 'ChangeTheme',
+            newTheme: e.target.value as Theme
+          })}}
+        >
+          <option value="base16-dark">base16-dark</option>
+          <option value="base16-light">base16-light</option>
+        </Select>
+      </div>
+
+      <div className="social-buttons">
+        <Button
+          className="font-license"
+          variant="link"
+          onClick={() => { window.open('https://github.com/ippee/programming_fonts_combination') }}
+        >
+          <i className="icon-github-circled" />
+        </Button>
+      </div>
     </div>
   );
 }
