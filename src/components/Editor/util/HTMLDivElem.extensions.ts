d203783c181589d19f8a780d70b0da86305ae7b9
ippee <ippee.music@gmail.com>
2021-05-26 12:21:46 +0900

    HTMLDivElementの拡張

---
+++ src/components/Editor/util/HTMLDivElem.extensions.ts
@@ -0,0 +1,17 @@
+import { CurrentFont, Lang } from "./typeAliases";
+
+declare global {
+  interface HTMLDivElement {
+    updateFont(currentFont: CurrentFont, isReverse: boolean): void;
+  }
+}
+
+HTMLDivElement.prototype.updateFont = function(currentFont: CurrentFont, isReverse: boolean) {
+  const k: Lang[] = ["eng", "jpn"];
+  if (isReverse) k.reverse();
+
+  const elem = this.children[0].children as HTMLCollectionOf<HTMLElement>;
+  elem[0].style.fontFamily = `"${currentFont[k[0]]}", "${
+    currentFont[k[1]]
+  }"`;
+};
--- src/components/Editor/util/typeAliases.ts
+++ src/components/Editor/util/typeAliases.ts
@@ -6,16 +6,3 @@ export type CurrentFont = {
   eng: string;
   jpn: string;
 };
-
-export class CodeMirrorElement extends HTMLDivElement {
-  updateFont(currentFont: CurrentFont, isReverse: boolean) {
-    const k: Lang[] = ["eng", "jpn"];
-    if (isReverse) k.reverse();
-
-    const elem = this.children[0].children as HTMLCollectionOf<HTMLElement>;
-    // Will reference the CodeMirror Element from the Virtual DOM.
-    elem[0].style.fontFamily = `"${currentFont[k[0]]}", "${
-      currentFont[k[1]]
-    }"`;
-  };
-}
