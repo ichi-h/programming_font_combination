912c1b3047cfe59f022263ae16df47baf474e3df
ippee <ippee.music@gmail.com>
2021-05-26 11:10:34 +0900

    フォントを更新するメソッドをHTMLDivElementに追加

--- src/components/Editor/util/typeAliases.ts
+++ src/components/Editor/util/typeAliases.ts
@@ -1,2 +1,21 @@
 export type Lang = "eng" | "jpn";
+
 export type Theme = "base16-dark" | "base16-light";
+
+export type CurrentFont = {
+  eng: string;
+  jpn: string;
+};
+
+export class CodeMirrorElement extends HTMLDivElement {
+  updateFont(currentFont: CurrentFont, isReverse: boolean) {
+    const k: Lang[] = ["eng", "jpn"];
+    if (isReverse) k.reverse();
+
+    const elem = this.children[0].children as HTMLCollectionOf<HTMLElement>;
+    // Will reference the CodeMirror Element from the Virtual DOM.
+    elem[0].style.fontFamily = `"${currentFont[k[0]]}", "${
+      currentFont[k[1]]
+    }"`;
+  };
+}
